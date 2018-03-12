a0a7e77a8008ec9f1aeb7632e68c97c7ba2d62da
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-03-12 13:27:56 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,37 @@
-    function displayTopicInfo() {
-      var topics = ["Woke", "Meta Physics", "The universe", "Spirituality"];
-      var topics = $(this).attr("data-topic");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=hHeUKZfzR6ic2S4M2kJJk0m8NVdAnkGL&limit=10";
+$("#button").on("click", function() {
 
+    var topic = $(this).attr("data-topic");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=hHeUKZfzR6ic2S4M2kJJk0m8NVdAnkGL&limit=10";
+            
     $.ajax({
       url: queryURL,
       method: "GET"
     })
       .then(function(response) {
-        console.log(queryURL);
-        console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < topics.length; i++) {
+
+          if (result[i].rating !== "r" && results[i].rating !== "pg-13"
+            ) {
+                var gifDiv = $("<div class='item'>");
+
+                  var rating = results[i].rating;
+                  
+                  var p = $("<p>").text("Rating: " + rating);
+                  
+                  var topicImage = $("<img>");
+                
+                  topicImage.attr("src", results[i].images.fixed_height.url);
+                  
+                  gifDiv.append(p);
+                  gifDiv.append(topicImage);
+                  
+                  $("#gifs-appear-here").prepend(gifDiv);
+                }
         
-      var results = response.data
-      
-      for (var i = 0; i < topic.length; i++) 
-      var gifDiv = $("<button>");
-
-      var rating = results[i].rating;
-      var p = $("<p>").text("Rating: " + rating);
-
-      var topicImage = $("<img>");
-      topicImage.attr("src", results[i].images.fixed_height.url);
-
-      gifImage.append(p);
-      gifImage.append(topicImage);
-      
-      $("#gifs-appear-here").prepend(gifDiv);
-
-      function renderButtons() {
-        $("button").on("click", function(event) {
-          event.preventDefault();
-          var topic = $("topic-input").val().trim();
-          topic.push(topic);
-          renderButtons(); 
-          $(document).on("click", "topic-btn", displayTopicInfo);
-      }
-    });
+              }
+            });
+          });
